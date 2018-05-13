394bf58aa148225792b9b5061e43229ccf62ecb1
burnstreet <andreas@tschritter.de>
2018-05-14 00:31:21 +0200

    implemented task 5 part 1

---
+++ src/main/java/CharGrouper.java
@@ -0,0 +1,38 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CharGrouper {
+
+  @Autowired
+  private ArraySorter sorter;
+  @Autowired
+  private StringCompressor compressor;
+
+  /*
+   * this satisfies the test in the task but will fail for multi-byte-characters.
+   */
+  public String group(String input) {
+    return compressor.compress(toString(sorter.sort(toIntArray(input))));
+  }
+
+
+  private int[] toIntArray(String input) {
+    byte[] bytes = input.getBytes();
+    int[] ints = new int[bytes.length];
+    for (int i = 0; i < bytes.length; i++) {
+      ints[i] = bytes[i];
+    }
+    return ints;
+  }
+
+
+  private String toString(int[] input) {
+
+    byte[] bytes = new byte[input.length];
+    for (int i = 0; i < bytes.length; i++) {
+      bytes[i] = (byte) input[i];
+    }
+    return new String(bytes);
+  }
+}
---
+++ src/test/java/CharGrouperTest.java
@@ -0,0 +1,62 @@
+import static org.testng.Assert.assertEquals;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+public class CharGrouperTest extends AbstractTestNGSpringContextTests {
+
+  @Autowired
+  private CharGrouper charGrouper;
+
+  @Test
+  public void groupUnsortedWithDuplicates() {
+    String input = "abzuaaissna";
+    String expected = "a4bins2uz";
+    assertEquals(charGrouper.group(input), expected);
+  }
+
+  @Test
+  public void groupUnsorted() {
+    String input = "zyx";
+    String expected = "xyz";
+    assertEquals(charGrouper.group(input), expected);
+  }
+
+  @Test
+  public void groupEmpty() {
+    String input = "";
+    assertEquals(charGrouper.group(input), input);
+  }
+
+  @Test(expectedExceptions = NullPointerException.class)
+  public void groupNull() {
+    String input = null;
+    //noinspection ConstantConditions
+    charGrouper.group(input);
+  }
+
+  /**
+   * Necessary for Spring DI to work
+   */
+  @Configuration
+  public static class Config {
+
+    @Bean
+    public static CharGrouper getCharGrouper() {
+      return new CharGrouper();
+    }
+
+    @Bean
+    public static StringCompressor getCompressor() {
+      return new StringCompressor();
+    }
+
+    @Bean
+    public static ArraySorter getSorter() {
+      return new ArraySorter();
+    }
+  }
+}
