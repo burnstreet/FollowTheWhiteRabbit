bb4d94c36605bf58a007b7860885b362d830cb23
burnstreet <andreas@tschritter.de>
2018-05-17 22:56:19 +0200

    remove obsolete code

--- src/test/java/de/tschritter/BonusTest.java
+++ src/test/java/de/tschritter/BonusTest.java
@@ -3,12 +3,9 @@ package de.tschritter;
 import static de.tschritter.Bonus.add;
 import static org.testng.Assert.assertEquals;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.testng.annotations.Test;
 
 public class BonusTest {
-
 
   @Test
   public void test() {
@@ -22,27 +19,5 @@ public class BonusTest {
     assertEquals((Integer) 3, i);
     i = add(i, 1);
     assertEquals((Integer) 4, i);
-  }
-
-  /**
-   * Necessary for Spring DI to work
-   */
-  @Configuration
-  public static class Config {
-
-    @Bean
-    public static CharGrouper getCharGrouper() {
-      return new CharGrouper();
-    }
-
-    @Bean
-    public static StringCompressor getCompressor() {
-      return new StringCompressor();
-    }
-
-    @Bean
-    public static ArraySorter getSorter() {
-      return new ArraySorter();
-    }
   }
 }
